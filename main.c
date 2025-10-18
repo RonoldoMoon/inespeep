b1e1ef3fb585e65f63f7665349c143e867db57ba
moon <zackware@gmail.com>
2025-10-18 16:04:58 -0700

    dump function half done

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include "cart.h"
 
 void usage();
+void infoDump(rom_header_t *p);
 
 int main(int argc, char* argv[])  {
 
@@ -20,6 +21,7 @@ int main(int argc, char* argv[])  {
 			exit(1);
 			}
 	
+	//read the header of file into ram
 	if ( (read(fd, &myHeader, sizeof(rom_header_t))) );
 		else {
 
@@ -27,6 +29,7 @@ int main(int argc, char* argv[])  {
 		exit(1);
 		}
 
+	//check signature duh!
 	if ( checkSignature(&myHeader) )  {
 
 	  puts("signature OK");
@@ -34,7 +37,9 @@ int main(int argc, char* argv[])  {
 
 	else { puts("Does not appear to be a valid iNES rom"); }
 
-	//dumpiNESHeader();
+	//dump header info to terminal.
+	putchar(10);
+	infoDump(&myHeader);
 
 	return 0;
 }
@@ -44,3 +49,29 @@ void usage()  {
 	puts("point me at a iNES ROM");
 	exit(1);
 }
+
+void infoDump(rom_header_t *p)  {
+
+	printf("number of 16kB program rom banks   - %d\n", p->prg_banks);
+	printf("number of  8kB character rom banks - %d\n", p->chr_banks);
+	printf("ram size                           - %dkB\n", p->ram_size*8);
+	printf("---\n");
+
+	if ( p->control_1 & 0x01 )  printf("verticle mirroring\n");
+	else printf("horizontle mirroring\n");
+
+	if ( p->control_1 & 0x02 )  printf("battery backed ram\n");
+	else printf("no battery backed ram\n");
+
+	if ( p->control_1 & 0x04 )  printf("trainer present\n");
+	else printf("no trainer present\n");
+
+	if ( p->control_1 & 0x08 )  printf("four screen VRAM layout\n");
+	else printf("not four screen VRAM layout\n");
+
+	/*
+	 * needs to look at other control byte, and dump mapper type.
+	 */
+
+}
+
